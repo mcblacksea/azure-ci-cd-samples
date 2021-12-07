e771923b7eec6b00d15a80a8af44594122a36802
anton <altecsyst@gmail.com>
2021-12-07 03:28:26 +0200

    fixed resources file

--- Cryptollet/Cryptollet/Common/Localization/Resources.ru-ru.Designer.cs
+++ Cryptollet/Cryptollet/Common/Localization/Resources.ru-ru.Designer.cs
@@ -22,14 +22,14 @@ namespace Cryptollet.Common.Localization {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class ResourcesRU {
+    public class Resources_ru_RU {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal ResourcesRU() {
+        internal Resources_ru_RU() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Cryptollet.Common.Localization {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Cryptollet.Common.Localization.ResourcesRU", typeof(ResourcesRU).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Cryptollet.Common.Localization.Resources.ru-RU", typeof(Resources_ru_RU).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
